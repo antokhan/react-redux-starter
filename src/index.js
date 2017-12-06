6745b0792582a6a7a80c19c008a9d5d27a29f11b
Md Nasir Uddin <mdantokhan@gmail.com>
2017-12-07 00:43:20 +0600

    Remove unwanted files and added redux and file structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './styles/styles.scss'
+import Main from './app/containers/main';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Main />, document.getElementById('root'));
 registerServiceWorker();
